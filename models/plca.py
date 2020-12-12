656b901a581ed36ee866ba32a151a88a72646311
eddie <aigagror@gmail.com>
2020-12-11 19:26:12 -0600

    temperature adjustment for proj-conv-plca

--- models/plca.py
+++ models/plca.py
@@ -130,7 +130,7 @@ class ProjConvPLCA(PLCA):
         super().__init__(channels, imsize, kern_size)
         self.nkern = nkern
 
-        self.temp = 1
+        self.temp = 0.01
 
         # Core parameters
         self.feats = nn.Parameter(torch.rand(nkern, channels, kern_size, kern_size))
